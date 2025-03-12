932987d118e30169463ecd2aec65f5f7cac91780
Ne'matov Saidakbar <Saidakbarnematov2509@gamil.com>
2025-03-12 14:46:53 +0500

    Update permissions

--- app_common/permissions.py
+++ app_common/permissions.py
@@ -2,16 +2,24 @@ from rest_framework.permissions import BasePermission
 
 class AdminUser(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         return request.user.is_authenticated and request.user.is_admin or request.user.is_staff
 
 class AdminOrTeacher(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         return request.user.is_authenticated and request.user.is_teacher or request.user.is_staff or request.user.is_admin
 
 class AdminOrStudent(BasePermission):
     def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
         return request.user.is_authenticated and request.user.is_student or request.user.is_staff or request.user.is_admin
 
 class AdminOrOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
+        if not request.user.is_authenticated:
+            return False
         return request.user.is_authenticated and obj.user == request.user or request.user.is_staff or request.user.is_admin
